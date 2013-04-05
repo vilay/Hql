100cb150da17500131e579c3f2d401999a892c9a
vilay <kandi.vilay09@gmail.com>
2013-04-05 22:25:19 +0530

    Removed unwanted file

--- src/Hql2.hs
+++ src/Hql2.hs
@@ -14,19 +14,15 @@ where
 
 
 --importing libraries 
-import Data.List
 import Database.HDBC
-import Database.HDBC.SqlValue
-import Data.Convertible.Base
 import Database.HDBC.Sqlite3
 import Data.Char
-import Data.Typeable
 
 --data types
 type HqlTable  = String
 type HqlDB     = String
 type HqlColumn = String 
-type HqlValues = String
+
  
 data HqlType = Int|Varchar|Bool|Double deriving (Show,Ord,Eq)
 
@@ -87,25 +83,25 @@ convertToHqlType strType = do
 execHqlSelectTable :: HqlTable -> [HqlColumn] -> IO ()
 
 execHqlSelectTable tabName colName  = do  
-                                                                                        --convert List of column to String.
-                                                                                        let col colName 
-                                                                                                                | colName == ["*"] = " * " 
-                                                                                                                | otherwise = foldl ( \ x y -> x ++ "," ++ y ) ( head colName ) ( tail colName )
-                                                                                        --create a query
-                                                                                        let query = "select " ++ col colName ++ " from " ++ tabName ++ ";"
-                                                                                        --database connection
-                                                                                        conn <- connectSqlite3 "test1.db";
-                                                                                        r <- quickQuery' conn query [];
-                                                                                        
-                                                                                        let   
-                                                                                              convRow :: [SqlValue] -> [String]
-                                                                                              x = (head r)
-                                                                                              len1 = length x -1
-                                                                                              convRow x = map (\ t -> fromSql $ ((!!) x t)::String ) [0..len1]
-                                                                                              stringRows = map convRow r                                                                                              
-                                                                                        print stringRows
-                                                                                        
-                                                                                        disconnect conn;
+                                        --convert List of column to String.
+                                        let col colName 
+                                                                | colName == ["*"] = " * " 
+                                                                | otherwise = foldl ( \ x y -> x ++ "," ++ y ) ( head colName ) ( tail colName )
+                                        --create a query
+                                        let query = "select " ++ col colName ++ " from " ++ tabName ++ ";"
+                                        --database connection
+                                        conn <- connectSqlite3 "test1.db";
+                                        r <- quickQuery' conn query [];
+                                        
+                                        let   
+                                              convRow :: [SqlValue] -> [String]
+                                              x = (head r)
+                                              len1 = length x -1
+                                              convRow x = map (\ t -> fromSql $ ((!!) x t)::String ) [0..len1]
+                                              stringRows = map convRow r                                                                                              
+                                        print stringRows
+                                        
+                                        disconnect conn;
 
 validateType :: HqlTable -> [HqlColumn] -> [HqlType] -> IO Bool
 validateType tabName colName types = do 
